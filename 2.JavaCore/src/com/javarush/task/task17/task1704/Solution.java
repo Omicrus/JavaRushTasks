ba1c3ba638ebda2d2e7cb96eb80e17f7b3bb475c
Omicrus <omicrus@gmail.com>
2018-09-27 11:19:28 +0200

    First commit

--- 2.JavaCore/src/com/javarush/task/task17/task1704/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1704/Solution.java
@@ -1,16 +1,15 @@
-package com.javarush.task.task17.task1703;
+package com.javarush.task.task17.task1704;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /* 
-Синхронизированные заметки
+Синхронизированные заметки 2
 */
 
 public class Solution {
 
     public static void main(String[] args) {
-
 
     }
 
@@ -18,29 +17,16 @@ public class Solution {
 
         public final List<String> notes = new ArrayList<String>();
 
-
-
-        public void addNote(int index, String note) {
+        public synchronized void addNote(int index, String note) {
             System.out.println("Сейчас будет добавлена заметка [" + note + "] На позицию " + index);
-            synchronized (notes) {
-                notes.add(index, note);
-            }
+            notes.add(index, note);
             System.out.println("Уже добавлена заметка [" + note + "]");
         }
 
-        public void removeNote(int index) {
+        public synchronized void removeNote(int index) {
             System.out.println("Сейчас будет удалена заметка с позиции " + index);
-
-                String note;
-            synchronized (notes) {
-                note = notes.remove(index);
-            }
+            String note = notes.remove(index);
             System.out.println("Уже удалена заметка [" + note + "] с позиции " + index);
-
         }
-
-
-
     }
-
 }
